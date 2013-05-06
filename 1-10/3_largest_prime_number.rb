2a4e24d97b367eea40387e3d39e5132858334c6a
robinhwang <binbinhuangcsu@gmail.com>
2013-05-06 19:42:04 +0800

    P3 V1.0

--- 1-10/3_largest_prime_number.rb
+++ 1-10/3_largest_prime_number.rb
@@ -2,5 +2,9 @@
 # 13195的素数约数是5，7，13，29    
 # 那么600851475143最大的素因数是？
 
-
-
+def find_big_num(x)
+  i = x/2
+  (2..i).each {|n| return find_big_num(x/n) if x%n == 0}
+  return x
+end
+puts find_big_num(600851475143)
