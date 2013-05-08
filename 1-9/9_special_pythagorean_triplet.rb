332d446263bb581238881975eb8ff113590aa575
robinhwang <binbinhuangcsu@gmail.com>
2013-05-08 12:25:42 +0800

    8,9 and more problem

--- 1-9/9_special_pythagorean_triplet.rb
+++ 1-9/9_special_pythagorean_triplet.rb
@@ -3,3 +3,13 @@
 #a**2 + b**2 = c**2
 #a + b + c = 1000
 #算出他们的乘积
+
+(1...1000).each do |a|
+  (1...1000).each do |b|
+    sqrt = Math.sqrt(a**2+ b**2)
+    if (sqrt.to_i==sqrt && (a + b + sqrt.to_i==1000))
+      puts a*b*(1000-a-b)
+    end
+  end
+end
+
